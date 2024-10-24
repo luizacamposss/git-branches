f3ddc9d0c7c49c85f0dca7c1f83014012f80faef
luizacamposss <mluizafields@gmail.com>
2024-10-24 17:29:13 +0000

    salvar

---
+++ principal.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls || clear")
+
+# git checkout -b criar-funcao-inss
+# git checkout -b feature-funcao-inss
+# git checkout -b funcionalidade-funcao-inss
+
+print("Adicionando funcionalidade: função inss.")
+
+def inss():
+    pass #serve para pular/considerar o codigo feito
